de8acdfd49a1c1f6013e5b4b40aed2f8a006d0b2
Joel <ajiolajoel@gmail.com>
2024-09-25 21:10:19 +0100

    currently making changes to the pricing section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link as LinkScroll } from "react-scroll";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { clsx } from "clsx";
 
 const NavLink = ({ title }) => (
   <LinkScroll
+    to={title}
+    offset={-100}
+    spy
+    smooth
+    activeClass="nav-active"
     className="base-bold text-p4 uppercase
     transition-colors duration-500 cursor-pointer hover:text-p1
     max-lg:my-4 max-lg:h-5"
@@ -14,12 +19,43 @@ const NavLink = ({ title }) => (
 );
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [islink, setIsLink] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsLink(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase
+    transition-colors duration-500 cursor-pointer hover:text-p1
+    max-lg:my-4 max-lg:h-5"
+    >
+      {title}
+    </LinkScroll>
+  );
 
   return (
     <header
-      className="fixed top-0 left-0 z-50
-        w-full py-10"
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+      )}
     >
       <div
         className="container flex h-14 items-center
@@ -62,7 +98,7 @@ const Header = () => {
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -84,7 +120,7 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
-
+            <div className="" />
             <div
               className="lg:hidden block absolute top-1/2 left-o
               w-[960px] h-[380px] translate-x-[-290px]
