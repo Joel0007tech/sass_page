cba05a5fb0e8810a1d344aa8a02480b3d2d42719
Joel <ajiolajoel@gmail.com>
2024-09-28 08:00:58 +0100

    still trying to build

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
              max-lg:h2 max-md:mb-4
               max-md:text-5xl max-md:leading-12"
             >
-              Amazing simple
+              Amazingly simple
             </h1>
             <p
               className="mb-14 max-w-440
